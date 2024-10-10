f1ab3f4d5ebb80a99127e2729fcf4d0ef56db796
Chif101 <augustolalo2008@gmail.com>
2024-10-10 12:43:02 -0300

    Add files via upload

---
+++ augustoversi/Atividade-js-curriculo/script.js
@@ -0,0 +1,18 @@
+const titulo = document.querySelector(".titulo")
+titulo.textContent = "Meu curriculo"
+titulo.style.textAlign = "Center"
+
+
+
+   const nome = document.getElementById("nome")
+   nome.textContent = "Nome: Augusto César"
+   nome.style.textAlign = "center"
+   
+   const idade = document.getElementById("idade")
+   idade.textContent = "idade: 16"
+   idade.style.textAlign = "center"
+
+   const img = document.getElementById("img")
+   img.style.height = "300px"
+   img.style.alignItems = "center"
+
