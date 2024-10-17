56f775fbd2b785fa61e0f2b5cb4938cba20f3194
Chif101 <augustolalo2008@gmail.com>
2024-10-17 12:42:33 -0300

    Add files via upload

---
+++ augustodia17cod/ula-18/script.js
@@ -0,0 +1,14 @@
+// function verificaMaiorIdade(idade){
+//     if(idade >=18){
+//       return alert("você é maior de idade")
+//     } else {
+//        return alert("você é menor de idade")
+//     }
+// }
+// verificaMaiorIdade(idade = Number(prompt("digite sua idade ")));
+
+function alterarBackGround(){
+
+    const body = document.querySelector("body")
+    body.style.backgroundColor = "#f2c2f5"
+}
